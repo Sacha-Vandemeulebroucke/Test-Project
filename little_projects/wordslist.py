110a9e2dbc11f4962c40164fb5a84809801d9fd4
Sacha-Vandemeulebroucke <145006455+Sacha-Vandemeulebroucke@users.noreply.github.com>
2025-02-01 16:47:41 +0100

    merge operation

---
+++ little_projects/wordslist.py
@@ -0,0 +1,9 @@
+words = (
+    "dog", "cat", "elephant", "tiger", "lion", "giraffe", "zebra", "horse", "donkey", "monkey",
+    "gorilla", "chimpanzee", "panda", "koala", "kangaroo", "rabbit", "fox", "wolf", "bear", "deer",
+    "moose", "buffalo", "cow", "sheep", "goat", "pig", "camel", "leopard", "cheetah", "hippopotamus",
+    "rhinoceros", "crocodile", "alligator", "turtle", "tortoise", "lizard", "snake", "frog", "toad", "salamander",
+    "dolphin", "whale", "shark", "octopus", "jellyfish", "crab", "lobster", "seahorse", "starfish", "penguin",
+    "eagle", "hawk", "falcon", "owl", "parrot", "peacock", "swan", "duck", "goose", "turkey",
+    "chicken", "rooster", "sparrow", "pigeon", "woodpecker", "flamingo", "bat", "rat", "mouse", "hedgehog"
+)
